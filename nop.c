0e6f853a60c872d00f572997e98f091e341861f9
UserBryan <bkk@gmail.com>
2023-12-15 08:11:44 -0800

    Nop opcode

--- nop.c
+++ nop.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
-  *func_nop- This doesn't do anything
-  *@head: This parameter is the stack head
-  *@line_number: This parameter is the counter
-  *Return: Void
+  *func_nop - This doesn't do anything.
+  *@head: This parameter is the stack head.
+  *@line_number: This parameter is the counter.
+  *Return: Void.
  */
 void func_nop(stack_t **head, unsigned int line_number)
 {
